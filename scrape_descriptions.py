2de447302873d2a7f119d82eb3518263f9342eab
griff1 <gpmaster117@gmail.com>
2015-02-20 17:36:00 -0800

    successfully returns a list containing job descriptions.  Known bug: some of the downloaded pages appear to have html errors, which makes the regex delete large sections of the description

--- scrape_descriptions.py
+++ scrape_descriptions.py
@@ -2,19 +2,36 @@ from bs4 import BeautifulSoup
 import re
 import urllib.request
 
-position = "software+engineer"
-location = "California"		#use "+" for spaces
-base_url = "http://www.indeed.com/jobs?q=" + position + "&l=" + location + "&start=";		# append 10, 20, 30... for more pages
+def get_page_ids(position="software+engineer", location="California", num_pages=1): 		#use "+" for spaces		
+	base_url = "http://www.indeed.com/jobs?q=" + position + "&l=" + location + "&start=";		# append 10, 20, 30... for more pages
 
-post_links = [];
+	post_links = [];
 
-for i in range(1):
-	sock = urllib.request.urlopen(base_url+str(i*10))
-	filename = sock.read() 
-	sock.close()
-	#soup = BeautifulSoup(filename)
-	#post_ids = soup.find_all(re.compile(r))
-	post_ids = re.findall("jobmap\[.\]= {jk:.'................", str(filename))
-	for i in range(len(post_ids)):
-		post_ids[i] = post_ids[i][17:]
-	print(post_ids)
+	for i in range(num_pages):
+		sock = urllib.request.urlopen(base_url+str((i+1)*10))
+		filename = sock.read() 
+		sock.close()
+		post_ids = re.findall("jobmap\[.\]= {jk:.'................", str(filename))
+		for i in range(len(post_ids)):
+			post_ids[i] = post_ids[i][17:]
+		return post_ids
+
+def get_listings(post_ids=get_page_ids()):
+	jobs = []
+	for p_id in post_ids:
+		sock = urllib.request.urlopen("http://www.indeed.com/viewjob?jk=" + p_id)
+		filename = sock.read()
+		sock.close()
+		soup = BeautifulSoup(filename)
+
+		title = soup.find("div", {"id": "job_header"})
+		title = re.sub("<.[^\<>]*>", "", str(title))	# strip out html tags
+		title = re.sub("<\.[^\<>]*>", "", str(title))	# strip out html tags
+
+		description = soup.find("span", {"id": "job_summary"})
+		description = re.sub("<.[^\<>]*>", "", str(description))	# strip out html tags
+		description = re.sub("<\.[^\<>]*>", "", str(description))	# strip out html tags
+
+		jobs.append(str(title)+str(description))
+
+	return jobs
