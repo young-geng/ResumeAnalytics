586a28c0e042ac440a93878e012e682ff3ac2190
griff1 <gpmaster117@gmail.com>
2015-02-20 17:48:59 -0800

    fixed bug where you couldn't modify number of pages to parse

--- scrape_descriptions.py
+++ scrape_descriptions.py
@@ -5,7 +5,7 @@ import urllib.request
 def get_page_ids(position="software+engineer", location="California", num_pages=1): 		#use "+" for spaces		
 	base_url = "http://www.indeed.com/jobs?q=" + position + "&l=" + location + "&start=";		# append 10, 20, 30... for more pages
 
-	post_links = [];
+	all_post_ids = [];
 
 	for i in range(num_pages):
 		sock = urllib.request.urlopen(base_url+str((i+1)*10))
@@ -14,7 +14,9 @@ def get_page_ids(position="software+engineer", location="California", num_pages=
 		post_ids = re.findall("jobmap\[.\]= {jk:.'................", str(filename))
 		for i in range(len(post_ids)):
 			post_ids[i] = post_ids[i][17:]
-		return post_ids
+		for i in post_ids:
+			all_post_ids.append(i)
+	return all_post_ids
 
 def get_listings(post_ids=get_page_ids()):
 	jobs = []
@@ -27,10 +29,12 @@ def get_listings(post_ids=get_page_ids()):
 		title = soup.find("div", {"id": "job_header"})
 		title = re.sub("<.[^\<>]*>", "", str(title))	# strip out html tags
 		title = re.sub("<\.[^\<>]*>", "", str(title))	# strip out html tags
+		title = re.sub("\n", "", str(title))	# strip out newlines
 
 		description = soup.find("span", {"id": "job_summary"})
 		description = re.sub("<.[^\<>]*>", "", str(description))	# strip out html tags
 		description = re.sub("<\.[^\<>]*>", "", str(description))	# strip out html tags
+		description = re.sub("\n", "", str(description))	# strip out html tags
 
 		jobs.append(str(title)+str(description))
 
