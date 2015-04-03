f4b705b4a51fa97b93c86b7a78355a2d4a11b635
young-geng <young.gengxy@gmail.com>
2015-04-03 10:46:58 -0700

    part of algorithm module completed

---
+++ server/algorithms/topic_model.py
@@ -0,0 +1,24 @@
+"""
+========================================================
+Topics extraction with Non-Negative Matrix Factorization
+========================================================
+"""
+
+
+from sklearn.decomposition import NMF
+
+
+"""
+Extract topics from sparse_matrix
+Return a pair (transformed_data, topics)
+"""
+def extract_topics(sparse_matrix, feature_names, n_topics=19, n_top_words=20):
+
+    nmf = NMF(n_components=n_topics, random_state=1)
+    transformed_data = nmf.fit_transform(sparse_matrix)
+
+    topics = []
+    for topic_idx, topic in enumerate(nmf.components_):
+        topics.append([feature_names[i] for i in topic.argsort()[:-n_top_words - 1:-1]])
+    return transformed_data, to_return
+
