ec3d11997a683f8c54a7b10797d42448aa13c531
young-geng <young.gengxy@gmail.com>
2015-05-08 02:43:36 -0700

    added db utils function template

---
+++ server/database/DBUtils.py
@@ -0,0 +1,34 @@
+import sqlite3
+
+
+
+""" Store the content of files into resumes database """
+def store_resumes(files):
+    pass
+
+
+
+""" Store the content of files into jobs database """
+def store_jobs(files):
+    pass
+
+
+""" Return a list of all resumes """
+def list_resumes():
+    pass
+
+""" Return a list of all jobs """
+def list_jobs():
+    pass
+
+
+
+""" Return a list of list, each inside list contains words for the topic """
+def get_topics():
+    pass
+
+
+
+""" Return a list of resumes as text file sorted by matchness with descending order """
+def find_matching_resume(jobid):
+    pass
